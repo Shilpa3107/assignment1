efcd07cb5f81316af1beb24d0ef6094d75b86862
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-10 01:53:14 +0530

    Installed the pg-nest

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModuleModule } from './user-module/user-module.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      password: 'abc123',
+      username: 'postgres',
+      entities: [],
+      database: 'pg-Nest',
+      synchronize: true,
+      logging: true,
+    }),
+     UserModuleModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
